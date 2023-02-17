e3f55ee32b1ba48304ebf8fc93ed0947be3a23fd
Abraham Bobmanuel <bobby.bbv@gmail.com>
2023-02-16 19:14:27 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - This is a C program that will print a line to the standard error
- * Return: 1 (It means it's succesful)
+ * Return: 1 (It means it's successful)
  */
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+        fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+        return (1);
 }

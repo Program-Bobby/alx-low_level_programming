9b8d32722c2ed2b63d69dbd2956d6d643ad3f216
Abraham Bobmanuel <bobby.bbv@gmail.com>
2023-02-16 18:40:00 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unixstd.h>
+/**
+ * main - This is a C program that will print a line to the standard error
+ * Return: 1 (It means it's succesful)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}

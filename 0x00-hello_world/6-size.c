184a3579ada7f660d6a5db112b08d1c3cb2255d6
Abraham Bobmanuel <bobby.bbv@gmail.com>
2023-02-16 17:18:13 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - A program that prints the size of various computer types
- * Return 0 (Success)
+ * main - A program that prints the size of various computer type
+ * Return 0 (Success made)
  */
 
 int main(void)

4fd11f045506253c8f70b0d8a608f84c9ea3396a
Abraham Bobmanuel <bobby.bbv@gmail.com>
2023-02-18 07:50:42 -0800

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,6 +14,7 @@ int main(void)
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
+
     if (n > 0)
     {
         printf("%d is positive\n", n);
@@ -22,10 +23,9 @@ int main(void)
     {
         printf("%d is zero\n", n);
     }
-    else
+    else if (n < 0);
     {
         printf("%d is negative\n", n);
     }
-
     return (0);
 }

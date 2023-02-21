5fde14b6d2cd08ea20d2f31dfd8528c43e44965d
Abraham Bobmanuel <bobby.bbv@gmail.com>
2023-02-21 10:44:48 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Entry point, print _putchar
+ *
+ * is written by BobbyAssists
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+return (0);
+}

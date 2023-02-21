f89b8d0677496ae85980b58bdf0c1eac63e9e751
Abraham Bobmanuel <bobby.bbv@gmail.com>
2023-02-21 11:06:31 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -17,5 +16,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
+_putchar('\n');
 return (0);
 }
